a133654b35664e019958128161d258850f117276
cheng38552@gmail.com <cheng38552@gmail.com>
2022-02-13 16:50:54 +0800

    初始化設定

--- Asp.Net MVC/App_Start/RouteConfig.cs
+++ Asp.Net MVC/App_Start/RouteConfig.cs
@@ -30,6 +30,7 @@ namespace Asp.Net_MVC
 //      url: "{controller}/{action}/{id}",
 //      defaults: new { controller = "Home", action= "Index", id = UrlParameter.Optional }
 //);
+
 1-1.
 When a request comes in,
 it's trying to do a pattern match based on
@@ -45,10 +46,41 @@ IIS Express will run
 HomeController Index action.
 It will map to Controllers/HomeController.cs
 and map to Index Method
+
 1-2.
 By convention in MVC.
 All controllers will have Controller suffix.
 This suffix is not requiredd in the URL.
 So, if you want to invoke Home controller,
 you specify /Home and not /HomeController.
+
+2.
+//routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+2.1.
+Reference:
+https://stackoverflow.com/questions/9016650/what-is-routes-ignorerouteresource-axd-pathinfo
+This line can handle the axd file request route, E.g. trace.axd
+.axd files don't exist physically.
+ASP.NET uses URLs with .axd extensions
+(ScriptResource.axd and WebResource.axd) internally,
+and they are handled by an HttpHandler.
+Therefore, you should keep this rule,
+to prevent ASP.NET MVC from trying to handle the request
+instead of letting the dedicated HttpHandler do it.
+
+2.2.
+trace.axd
+Reference:
+https://msdn.microsoft.com/en-us/library/wwh16c6c.aspx
+trace.axd trace details for a specific request.
+If you want to enable trace.axd,
+then you have to go to Web.config
+Add <trace enabled="true" pageOutput="false"/> under <system.web>
+Then run the project, type the following URL
+http://localhost/YourProjectName/trace.axd
+This will return ASP.NET trace, trace.axd.
+If you do not have
+// routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+then you can not enable the trace.axd.
 */
---
+++ Asp.Net MVC/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace OnlineGame.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return "Hellow";
+        }
+    }
+}
