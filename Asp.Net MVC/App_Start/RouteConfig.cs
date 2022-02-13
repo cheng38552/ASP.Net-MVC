9146cb9105257c4cc0aed7af071186b92d4196f3
cheng38552@gmail.com <cheng38552@gmail.com>
2022-02-13 16:26:30 +0800

    專案建立

---
+++ Asp.Net MVC/App_Start/RouteConfig.cs
@@ -0,0 +1,54 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Asp.Net_MVC
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            //Handle the Route of the axd request file.
+            //E.g. ASP.Net Tracing
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            //Handle the Route called "Default".
+            //The mapping URL is "{controller}/{]action}/{id}"
+            //Set the default value of Controller, action, and id.
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
+
+/*
+1.
+//routes.MapRoute(
+//      name: "Default",
+//      url: "{controller}/{action}/{id}",
+//      defaults: new { controller = "Home", action= "Index", id = UrlParameter.Optional }
+//);
+1-1.
+When a request comes in,
+it's trying to do a pattern match based on
+all the templates it sees in these mapped routes.
+A route is some instrucions for
+how to take a URI coming into a request
+and map it to some code,
+normally a controller.
+In this case,
+look at defaults parameter,
+when user request Http://localhost:PortNumber/
+IIS Express will run
+HomeController Index action.
+It will map to Controllers/HomeController.cs
+and map to Index Method
+1-2.
+By convention in MVC.
+All controllers will have Controller suffix.
+This suffix is not requiredd in the URL.
+So, if you want to invoke Home controller,
+you specify /Home and not /HomeController.
+*/
