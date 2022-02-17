ac3003c9d1c7678074216d19a17ad2bebd22e522
cheng38552@gmail.com <cheng38552@gmail.com>
2022-02-18 00:44:01 +0800

    BusinessLayer

--- Asp.Net MVC/Controllers/GamerController.cs
+++ Asp.Net MVC/Controllers/GamerController.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
+using BusinessLayer;
 using OnlineGame.Web.Data;
-using OnlineGame.Web.Models;
+using Gamer = OnlineGame.Web.Models.Gamer;
 namespace OnlineGame.Web.Controllers
 {
     public class GamerController : Controller
@@ -19,6 +21,7 @@ namespace OnlineGame.Web.Controllers
         //    };
         //    return View(gamer);
         //}
+
         // http://localhost/OnlineGame.Web/Gamer/Details
         // http://localhost/OnlineGame.Web/Gamer/Details/1
         // http://localhost/OnlineGame.Web/Gamer/Details/2
@@ -46,15 +49,119 @@ namespace OnlineGame.Web.Controllers
             }
             return View(gamer);
         }
+
         public ActionResult Index(int teamId)
         {
+            //Entity Framework
             OnlineGameContext context = new OnlineGameContext();
             List<Gamer> gamers = context.Gamers.Where(gamer => gamer.TeamId == teamId).ToList();
             return View(gamers);
         }
+
+        public ActionResult Index2()
+        {
+            //Ado.Net
+            GamerBusinessLayer gamerBusinessLayer = new GamerBusinessLayer();
+            List<BusinessLayer.Gamer> gamers = gamerBusinessLayer.Gamers.ToList();
+            return View(gamers);
+        }
+
+        //[HttpGet] attribute means it only respond to the "GET" request.
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // 1. Retrieve form data using FormCollection
+        [HttpPost]
+        public ActionResult Create(FormCollection formCollection)
+        {
+            ////FormCollection implement C# indexer.
+            ////See each key and value of formCollection.
+            //foreach (string key in formCollection.AllKeys)
+            //{
+            //    Response.Write($"key=={key}, {formCollection[key]}, <br/>");
+            //}
+            int teamId;
+            BusinessLayer.Gamer gamer = new BusinessLayer.Gamer
+            {
+                Name = formCollection["Name"],
+                Gender = formCollection["Gender"],
+                City = formCollection["City"],
+                DateOfBirth = Convert.ToDateTime(formCollection["DateOfBirth"]),
+                TeamId = int.TryParse(formCollection["TeamId"], out teamId) ? teamId : 0
+            };
+            GamerBusinessLayer gamerBusinessLayer =
+                new GamerBusinessLayer();
+            gamerBusinessLayer.AddGamer(gamer);
+            return RedirectToAction("Index2");
+        }
+
+        // 2. Retrieve form data using name attribute of input tag from cshtml
+        [HttpPost]
+        public ActionResult Create2(string name, string gender, string city, DateTime dateOfBirth, int teamId)
+        {
+            BusinessLayer.Gamer gamer = new BusinessLayer.Gamer
+            {
+                Name = name,
+                Gender = gender,
+                City = city,
+                DateOfBirth = dateOfBirth,
+                TeamId = teamId
+            };
+            GamerBusinessLayer gamerBusinessLayer =
+                new GamerBusinessLayer();
+            gamerBusinessLayer.AddGamer(gamer);
+            return RedirectToAction("Index2");
+        }
+
+        // 3. Retrieve form data using model binding
+        [HttpPost]
+        public ActionResult Create3(BusinessLayer.Gamer gamer)
+        {
+            //if any of input is not valid.
+            if (!ModelState.IsValid)
+            {
+                return View("Create");
+                //Go to Create.cshtml,
+                //so users can correct their input value.
+            }
+            GamerBusinessLayer gamerBusinessLayer =
+                new GamerBusinessLayer();
+            gamerBusinessLayer.AddGamer(gamer);
+            return RedirectToAction("Index2");
+        }
+
+        // 4. Retrieve form data using model binding by UpdateModel() or TryUpdateModel()
+        [HttpPost]
+        [ActionName("Create4")]
+        public ActionResult Create_Post()
+        {
+            //if any of input is not valid.
+            if (!ModelState.IsValid)
+            {
+                return View("Create");
+                //Go to Create.cshtml,
+                //so users can correct their input value.
+            }
+            GamerBusinessLayer gamerBusinessLayer =
+                new GamerBusinessLayer();
+            BusinessLayer.Gamer gamer = new BusinessLayer.Gamer();
+            //UpdateModel<BusinessLayer.Gamer>(gamer);
+            //UpdateModel(gamer);
+            TryUpdateModel(gamer);
+            //1.
+            // UpdateModel() and TryUpdateModel() inspects all the HttpRequest inputs
+            // such as posted Form data, QueryString,
+            // Cookies and Server variables and populate the gamer object.
+            gamerBusinessLayer.AddGamer(gamer);
+            return RedirectToAction("Index2");
+        }
     }
 }
 /*
+1.
 //var onlineGameContext = new OnlineGameContext();
 //Gamer gamer = onlineGameContext.Gamers.Single(p => p.Id == id);
 When user request, EntityFramework will request the data from the database
@@ -67,4 +174,68 @@ Thus, each Gamer Model object is a temp place to store each Gamer Table entity f
 Then we pass the Gamer Model object as the ViewModel,
 Thus, the Details.cshtml view can use the values from Gamer Model object
 which is actually the temp place to store Gamer Table entity data.
+-------------------------------
+2.
+//[HttpGet]
+//public ActionResult Create()
+The GET request will direct to Views/Gamer/Create.cshtml.
+-------------------------------
+3.
+//[HttpPost]
+-----------------
+3.1.
+//[HttpPost]
+//public ActionResult Create(FormCollection formCollection)
+Retrieve form data using FormCollection.
+The key is the name attribute of input or select tag from cshtml.
+-----------------
+3.2.
+//[HttpPost]
+//public ActionResult Create2(string name, string gender, string city, DateTime dateOfBirth, int teamId)
+Retrieve form data using name attribute of input tag from cshtml.
+3.2.1.
+string name is from
+//<input class="form-control text-box single-line" id="Name" name="Name" type="text" value="">
+3.2.2.
+string gender is from
+//<select id="Gender" name="Gender">...</select>
+3.2.3.
+string city is from
+<input class="form-control text-box single-line" id="City" name="City" type="text" value="">
+3.2.4.
+DateTime dateOfBirth is from
+//<input class="form-control text-box single-line" data-val="true" data-val-date="The field DateOfBirth must be a date." data-val-required="The DateOfBirth field is required." id="DateOfBirth" name="DateOfBirth" type="datetime" value="">
+3.2.5.
+int teamId is from
+//<input class="form-control text-box single-line" data-val="true" data-val-number="The field TeamId must be a number." data-val-required="The TeamId field is required." id="TeamId" name="TeamId" type="number" value="">
+-----------------
+3.3.
+//[HttpPost]
+//public ActionResult Create3(BusinessLayer.Gamer gamer)
+If the view has a lot of input,
+then the previous two ways is not a good idea.
+It is always better to retrieve form data using model binding.
+The model of the cshtml is BusinessLayer.Gamer,
+so we can pass the model object into HttpPost action.
+The property value of model object will contain the value
+from input or select tag from cshtml based on name attribute.
+-----------------
+3.4.
+//[HttpPost]
+//[ActionName("Create4")]
+//public ActionResult Create_Post()
+Retrieve form data using model binding by UpdateModel() or TryUpdateModel()
+...
+//BusinessLayer.Gamer gamer = new BusinessLayer.Gamer();
+////UpdateModel<BusinessLayer.Gamer>(gamer);
+////UpdateModel(gamer);
+//TryUpdateModel(gamer);
+3.4.1.
+UpdateModel() and TryUpdateModel() inspects all the HttpRequest inputs
+such as posted Form data, QueryString,
+Cookies and Server variables and populate the gamer object.
+3.4.2.
+UpdateModel() throws an exception if validation fails.
+TryUpdateModel() will never throw an exception and
+return false if validation fails.
 */
---
+++ BusinessLayer/GamerBusinessLayer.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BusinessLayer
+{
+    public class GamerBusinessLayer
+    {
+        public IEnumerable<Gamer> Gamers
+        {
+            get
+            {
+                string connectionString =
+                    ConfigurationManager.ConnectionStrings["OnlineGameContext"].ConnectionString;
+                List<Gamer> gamers = new List<Gamer>();
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    SqlCommand cmd = new SqlCommand("spGetGamers", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    con.Open();
+                    SqlDataReader rdr = cmd.ExecuteReader();
+                    while (rdr.Read())
+                    {
+                        Gamer gamer = new Gamer();
+                        gamer.Id = Convert.ToInt32(rdr["Id"]);
+                        gamer.Name = rdr["Name"].ToString();
+                        gamer.Gender = rdr["Gender"].ToString();
+                        gamer.City = rdr["City"].ToString();
+                        gamer.DateOfBirth = Convert.ToDateTime(rdr["DateOfBirth"]);
+                        gamer.TeamId = Convert.ToInt32(rdr["TeamId"]);
+                        gamers.Add(gamer);
+                    }
+                }
+                return gamers;
+            }
+        }
+        public void AddGamer(Gamer gamer)
+        {
+            string connectionString =
+            ConfigurationManager.ConnectionStrings["OnlineGameContext"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("spAddGamer", con)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+                cmd.Parameters.Add(CreateSqlParameter("@Name",gamer.Name));
+                cmd.Parameters.Add(CreateSqlParameter("@Gender", gamer.Gender));
+                cmd.Parameters.Add(CreateSqlParameter("@City", gamer.City));
+                cmd.Parameters.Add(CreateSqlParameter("@DateOfBirth", gamer.DateOfBirth));
+                cmd.Parameters.Add(CreateSqlParameter("@TeamId", gamer.TeamId));
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+        SqlParameter CreateSqlParameter(string name,object value)
+        {
+            return new SqlParameter
+            {
+                ParameterName = name,
+                Value = value
+            };
+        }
+    }
+}
